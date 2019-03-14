016f16bea91c77c8c86a6ec1ca73d80a68d031b5
Natalia PN <npelegero@gmail.com>
2019-03-14 18:23:31 +0100

    Add button for submitting form

--- src/components/AddBooks.js
+++ src/components/AddBooks.js
@@ -4,19 +4,23 @@ import React, { Component } from 'react';
 class AddBooks extends Component {
     render() {
         return(
-            <form className="Add-books__form">
-                <label className="Form__title-label">Title: </label>
-                <input type="text"/> 
+            <div className="Form__container">
+                <form className="Add-books__form">
+                    <label className="Form__title-label">Title: </label>
+                    <input type="text"/> 
 
-                <label className="Form__author-label">Author: </label>
-                <input type="text"/> 
+                    <label className="Form__author-label">Author: </label>
+                    <input type="text"/> 
 
-                <label className="Form__genre-label">Genre: </label>
-                <input type="text"/> 
+                    <label className="Form__genre-label">Genre: </label>
+                    <input type="text"/> 
 
-                <label className="Form__price-label">Price: </label>
-                <input type="text"/> 
-            </form>
+                    <label className="Form__price-label">Price: </label>
+                    <input type="text"/> 
+                </form>
+                <label className="send-books__label"></label>
+                <input type="submit" className="Send-books__button"/>
+            </div>
         );   
     }
 }
